a759a1f0a5970eb7960574189c59c0e5c115fd1c
imane-mbarek <mane.aytmbarek@gmail.com>
2025-07-20 21:59:20 +0100

    Reusable Query Context Manager

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -11,3 +11,10 @@ class ExecuteQuery(self,query , param):
        def __exit__(self,exc_type, exc_val,exc_tab):
            self.conn.commit()
            self.conn.close()
+
+
+if __name__ == "__main__":
+    query = "SELECT * FROM users WHERE age > ?"
+    with ExecuteQuery(query, 25) as results:
+        for row in results:
+            print(row)
