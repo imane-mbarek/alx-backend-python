ee76a1e2f3db319f43fe322657d7ca02707a081a
imane-mbarek <mane.aytmbarek@gmail.com>
2025-07-21 18:03:20 +0100

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,14 +8,14 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
 
-@parameterized.expand([
+      @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
-def test_access_nested_map(self,nested_map,path, expected):
-   result= self.assertEqual(access_nested_map(nested_map,path),expected)
-   return result
+      ])
+      def test_access_nested_map(self,nested_map,path, expected):
+        self.assertEqual(access_nested_map(nested_map,path),expected)
+   
 
 
 
