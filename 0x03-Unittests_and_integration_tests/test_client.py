d2eb235ebe8e882fa9320f8dd3a23d2d36c0ff23
imane-mbarek <mane.aytmbarek@gmail.com>
2025-07-22 19:42:01 +0100

    Parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -14,7 +14,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",),
         ("abc",)
     ])
-      @patch('client.get_json')
+      @patch('client.GithubOrgClient.get_json')
       def test_org(self, org_name, mock_get_json):
 
           # Préparer une fausse réponse de get_json
